ebe2cd4d328b98ce95ffad2c41af5f12ed39edd3
DJV <djtvaq@gmail.com>
2022-07-19 22:02:33 -0400

    declare function that generates computers choice

--- script.js
+++ script.js
@@ -5,3 +5,12 @@
 // We will return a line that says whether the player lost or won.
 // We will loop this FIVE times, to make a game that is best out of five.
 // At the end a winner will be crowned. 
+
+let choices = ['rock', 'paper', 'scissors']
+
+function getComputerChoice() {
+    let computerChoice = choices[Math.floor(Math.random() * 3)]
+    return computerChoice
+}
+
+console.log(getComputerChoice())
