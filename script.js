97c0c80613e4fcb10beec61fc085789b1d4b10d6
DJV <djtvaq@gmail.com>
2022-07-19 21:54:06 -0400

    Write out the pseudocode for the game function

---
+++ script.js
@@ -0,0 +1,7 @@
+// Psuedocode
+// The computer will choose randomly between rock, paper or scissors
+// The player will input one of the three choices. 
+// We will compare the choices and decide who is the winner.
+// We will return a line that says whether the player lost or won.
+// We will loop this FIVE times, to make a game that is best out of five.
+// At the end a winner will be crowned. 
